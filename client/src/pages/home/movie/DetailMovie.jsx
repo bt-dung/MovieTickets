94a9df74f449121e6ec4ae7018079e1b6715b0ec
DinhMinh2963 <dxminh2963@gmail.com>
2025-03-03 21:04:20 +0700

    3/3/2025 #1

--- client/src/pages/home/movie/DetailMovie.jsx
+++ client/src/pages/home/movie/DetailMovie.jsx
@@ -1,5 +1,244 @@
 import React from "react";
 const DetailMovie = () => {
-    return (<><div>DetailMovie</div></>);
+  return (
+    <>
+      <section
+        class="details-banner bg_img"
+        data-background="../../../assets/images/banner/banner03.jpg"
+      >
+        <div class="container">
+          <div class="details-banner-wrapper">
+            <div class="details-banner-thumb">
+              <img src="../../../assets/images/movie/venus.jpg" alt="movie" />
+              <a
+                href="https://www.youtube.com/embed/KGeBMAgc46E"
+                class="video-popup"
+              >
+                <img
+                  src="../../../assets/images/movie/video-button.png"
+                  alt="movie"
+                />
+              </a>
+            </div>
+            <div class="details-banner-content offset-lg-3">
+              <h3 class="title">Venus</h3>
+              <div class="tags">
+                <a href="#0">English</a>
+                <a href="#0">Hindi</a>
+                <a href="#0">Telegu</a>
+                <a href="#0">Tamil</a>
+              </div>
+              <a href="#0" class="button">
+                horror
+              </a>
+              <div class="social-and-duration">
+                <div class="duration-area">
+                  <div class="item">
+                    <i class="fas fa-calendar-alt"></i>
+                    <span>06 Dec, 2020</span>
+                  </div>
+                  <div class="item">
+                    <i class="far fa-clock"></i>
+                    <span>2 hrs 50 mins</span>
+                  </div>
+                </div>
+                <ul class="social-share">
+                  <li>
+                    <a href="#0">
+                      <i class="fab fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#0">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#0">
+                      <i class="fab fa-pinterest-p"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#0">
+                      <i class="fab fa-linkedin-in"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#0">
+                      <i class="fab fa-google-plus-g"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="book-section bg-one">
+        <div class="container">
+          <div class="book-wrapper offset-lg-3">
+            <div class="left-side">
+              <div class="item">
+                <div class="item-header">
+                  <div class="thumb">
+                    <img
+                      src="../../../assets/images/movie/tomato2.png"
+                      alt="movie"
+                    />
+                  </div>
+                  <div class="counter-area">
+                    <span
+                      class="counter-item odometer"
+                      data-odometer-final="88"
+                    >
+                      0
+                    </span>
+                  </div>
+                </div>
+                <p>tomatometer</p>
+              </div>
+              <div class="item">
+                <div class="item-header">
+                  <div class="thumb">
+                    <img
+                      src="../../../assets/images/movie/cake2.png"
+                      alt="movie"
+                    />
+                  </div>
+                  <div class="counter-area">
+                    <span
+                      class="counter-item odometer"
+                      data-odometer-final="88"
+                    >
+                      0
+                    </span>
+                  </div>
+                </div>
+                <p>audience Score</p>
+              </div>
+            </div>
+            <a href="#0" class="custom-button">
+              book tickets
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section class="movie-details-section padding-top padding-bottom">
+        <div class="container">
+          <div class="row justify-content-center flex-wrap-reverse mb--50">
+            <div class="col-lg-3 col-sm-10 col-md-6 mb-50">
+              <div class="widget-1 widget-tags">
+                <ul>
+                  <li>
+                    <a href="#0">2D</a>
+                  </li>
+                  <li>
+                    <a href="#0">imax 2D</a>
+                  </li>
+                  <li>
+                    <a href="#0">4DX</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="widget-1 widget-offer">
+                <h3 class="title">Applicable offer</h3>
+                <div class="offer-body">
+                  <div class="offer-item">
+                    <div class="thumb">
+                      <img
+                        src="../../../assets/images/sidebar/offer01.png"
+                        alt="sidebar"
+                      />
+                    </div>
+                    <div class="content">
+                      <h6>
+                        <a href="#0">Amazon Pay Cashback Offer</a>
+                      </h6>
+                      <p>Win Cashback Upto Rs 300*</p>
+                    </div>
+                  </div>
+                  <div class="offer-item">
+                    <div class="thumb">
+                      <img
+                        src="../../../assets/images/sidebar/offer02.png"
+                        alt="sidebar"
+                      />
+                    </div>
+                    <div class="content">
+                      <h6>
+                        <a href="#0">PayPal Offer</a>
+                      </h6>
+                      <p>
+                        Transact first time with Paypal and get 100% cashback up
+                        to Rs. 500
+                      </p>
+                    </div>
+                  </div>
+                  <div class="offer-item">
+                    <div class="thumb">
+                      <img
+                        src="../../../assets/images/sidebar/offer03.png"
+                        alt="sidebar"
+                      />
+                    </div>
+                    <div class="content">
+                      <h6>
+                        <a href="#0">HDFC Bank Offer</a>
+                      </h6>
+                      <p>
+                        Get 15% discount up to INR 100* and INR 50* off on F&B
+                        T&C apply
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="widget-1 widget-banner">
+                <div class="widget-1-body">
+                  <a href="#0">
+                    <img
+                      src="../../../assets/images/banner/banner13.jpg"
+                      alt="banner"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-9 mb-50">
+              <div class="movie-details">
+                <div class="tab summery-review">
+                  <ul class="tab-menu">
+                    <li class="active">summery</li>
+                  </ul>
+                  <div class="tab-area">
+                    <div class="tab-item active">
+                      <div class="item">
+                        <h5 class="sub-title">Synopsis</h5>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Proin vehicula eros sit amet est tincidunt
+                          aliquet. Fusce laoreet ligula ac ultrices eleifend.
+                          Donec hendrerit fringilla odio, ut feugiat mi
+                          convallis nec. Fusce elit ex, blandit vitae mattis sit
+                          amet, iaculis ac elit. Ut diam mauris, viverra sit
+                          amet dictum vel, aliquam ac quam. Ut mi nisl,
+                          fringilla sit amet erat et, convallis porttitor
+                          ligula. Sed auctor, orci id luctus venenatis, dui
+                          dolor euismod risus, et pharetra orci lectus quis
+                          sapien. Duis blandit ipsum ac consectetur scelerisque.{" "}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 export default DetailMovie;
